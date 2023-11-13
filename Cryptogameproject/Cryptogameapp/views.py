c0f4e5804f8ee3c3b6c1c41bd1546c5652a13958
Jane <Jannyroux@yandex.ru>
2023-11-13 20:32:22 +0300

    EvgeniyaKruglova

--- Cryptogameproject/Cryptogameapp/views.py
+++ Cryptogameproject/Cryptogameapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import StudyCard, TaskCard
-
+from .forms import TaskForm
 def main_page (request):
     study = StudyCard.objects.all()
     paginator = Paginator(study,3)
@@ -15,11 +15,27 @@ def main_page (request):
         request,
         'main_page.html',
         context=
-        {'study':study, 'tasks':tasks, 'page_obj-1': page_obj_1,'page_obj-2': page_obj_2, },
+        {'study':study, 'tasks':tasks, 'page_obj-1': page_obj_1,'page_obj-2': page_obj_2}
 
     )
 
 
+def task_list(request):
+    tasks = TaskCard.objects.all().order_by('published.strftime("%Y-%m")')
+    return render(
+        request,
+        'task_list.html',
+        context={'tasks': tasks})
 
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'task_create.html', {'form': form})
 
 
--- Cryptogameproject/Cryptogameproject/settings.py
+++ Cryptogameproject/Cryptogameproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Cryptogameapp.apps.CryptogameappConfig',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
