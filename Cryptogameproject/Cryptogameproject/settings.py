3ccb55042a721b78ee5295f6d6e5a58a5400cb0c
Jane <Jannyroux@yandex.ru>
2023-11-15 21:42:42 +0300

    EvgeniyaKruglova

--- Cryptogameproject/Cryptogameproject/settings.py
+++ Cryptogameproject/Cryptogameproject/settings.py
@@ -131,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
