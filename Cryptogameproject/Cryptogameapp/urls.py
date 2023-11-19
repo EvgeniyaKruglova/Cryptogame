00fe0a0cb395d7f809b3aa950808d23369e5802f
Jane <Jannyroux@yandex.ru>
2023-11-19 20:56:33 +0300

    EvgeniyaKruglova

--- Cryptogameproject/Cryptogameapp/urls.py
+++ Cryptogameproject/Cryptogameapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import main_page, task_list, task_create
+from .views import main_page, task_detail,task_list, task_create, task_edit, task_delete
 
 urlpatterns = [
     path('', main_page, name='main_page'),
+    path('task/<int:pk>/', task_detail, name='task_detail'),
     path('tasklist', task_list, name='task_list'),
-    path('create/', task_create, name='task_create')
-
+    path('create/', task_create, name='task_create'),
+    path('task/<int:pk>/edit/', task_edit, name='task_edit'),
+    path('task/<int:pk>/delete/', task_delete, name='task_delete'),
 ]
--- Cryptogameproject/Cryptogameapp/views.py
+++ Cryptogameproject/Cryptogameapp/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from .models import StudyCard, TaskCard
 from .forms import TaskForm
 from .filters import  TaskCardFilter
+from django.shortcuts import get_object_or_404
 def main_page (request):
     study = StudyCard.objects.all()
     paginator = Paginator(study,3)
@@ -29,6 +30,13 @@ def task_list(request):
         'task_list.html',
         context={'tasks': tasks})
 
+def task_detail(request,pk):
+    task= get_object_or_404(TaskCard,pk=pk)
+    return render(
+        request,
+        'task_detail.html',
+        context={'task': task})
+
 
 def task_create(request):
     if request.method == 'POST':
@@ -41,3 +49,20 @@ def task_create(request):
     return render(request, 'task_create.html', {'form': form})
 
 
+def task_edit(request, pk):
+    task = get_object_or_404(TaskCard, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'task_edit.html', {'form': form})
+
+
+def task_delete(request, pk):
+    task = get_object_or_404(TaskCard, pk=pk)
+    task.delete()
+    return redirect('task_list')
