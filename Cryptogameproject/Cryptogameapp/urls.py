e71cf283539070b5ec39dc832c473cd63807d00f
Tokarev Alexandr <tokarewalexandr33@gmail.com>
2023-11-21 22:07:01 +0400

    TokarevAlexandr

--- Cryptogameproject/Cryptogameapp/urls.py
+++ Cryptogameproject/Cryptogameapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import main_page, task_detail,task_list, task_create, task_edit, task_delete
+from .views import (
+    main_page, task_detail,task_list, task_create, task_edit, task_delete, PartnerList, PartnerDetail
+)
 
 urlpatterns = [
     path('', main_page, name='main_page'),
@@ -8,4 +10,6 @@ urlpatterns = [
     path('create/', task_create, name='task_create'),
     path('task/<int:pk>/edit/', task_edit, name='task_edit'),
     path('task/<int:pk>/delete/', task_delete, name='task_delete'),
+    path('', PartnerList.as_view()),
+    path('<int:pk>', PartnerDetail.as_view()),
 ]
--- Cryptogameproject/Cryptogameapp/views.py
+++ Cryptogameproject/Cryptogameapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from rest_framework import filters
 from django.core.paginator import Paginator
-from .models import StudyCard, TaskCard
+from .models import StudyCard, TaskCard, Partner
 from .forms import TaskForm
 from .filters import  TaskCardFilter
 from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 def main_page (request):
     study = StudyCard.objects.all()
     paginator = Paginator(study,3)
@@ -66,3 +67,14 @@ def task_delete(request, pk):
     task = get_object_or_404(TaskCard, pk=pk)
     task.delete()
     return redirect('task_list')
+
+class PartnerList(ListView):
+    model = Partner
+    ordering = 'name'
+    template_name = 'partners.html'
+    context_object_name = 'partners'
+
+class PartnerDetail(DetailView):
+    model = Partner
+    template_name = 'partner.html'
+    context_object_name = 'partner'
