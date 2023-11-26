69ea77c0a7fbadf4509e3bf772a79c5b0ee1e09d
Jane <Jannyroux@yandex.ru>
2023-11-26 11:02:06 +0300

    EvgeniyaKruglova

--- Cryptogameproject/Cryptogameproject/settings.py
+++ Cryptogameproject/Cryptogameproject/settings.py
@@ -77,7 +77,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
-                'allauth.account.middleware.AccountMiddleware',
             ],
         },
     },
